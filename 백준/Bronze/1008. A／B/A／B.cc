24a09c72e306067eb63c814607596f2f5a61779c
YunSeok Choi <tnvjdbs@naver.com>
2025-10-29 18:17:25 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+    //unsigned int A;
+    //signed int B;
+    double A;
+	double B;
+
+    scanf("%lf %lf", &A, &B);
+    printf("%.9f", A/B);
+
+
+    return 0;
+}
