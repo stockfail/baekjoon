2fd2e462c338cd6a9e124b518ab8b25877c9d321
YunSeok Choi <tnvjdbs@naver.com>
2025-10-29 18:38:15 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    unsigned int A;
+    signed int B;
+
+    scanf("%d %d", &A, &B);
+    printf("%d \n", A+B);
+    printf("%d \n", A-B);
+    printf("%d \n", A*B);
+    printf("%d \n", A/B);
+    printf("%d \n", A%B);
+
+    return 0;
+}
