c69b3de44b0d19eccbece700717ea69d7fe8eb2e
YunSeok Choi <tnvjdbs@naver.com>
2025-11-10 01:14:49 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+  char C;
+  int X;
+  scanf("%c", &C);
+  X = C;
+  printf("%d", X);
+    return 0;
+}
+
